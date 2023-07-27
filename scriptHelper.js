74694e5e10cfc5cddcd2b83d029631e77f762041
Cory <standardvr@gmail.com>
2023-07-26 20:14:26 -0500

    Added specifications on color

--- scriptHelper.js
+++ scriptHelper.js
@@ -56,13 +56,13 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
         list.style.visibility = 'visible';
         launchStatus.innerHTML = `Shuttle Not Ready for Launch`;
-        launchStatus.style.color = 'red';
+        launchStatus.style.color = 'rgb(199, 37, 78)';
     }else if(cargoLevel < 10000 && fuelLevel >= 10000) {
         list.style.visibility = 'visible';
         fuelStatus.innerHTML = `Fuel level high enough for launch`;
         cargoStatus.innerHTML = `Cargo mass low enough for launch`;
         launchStatus.innerHTML = `Shuttle Ready For Launch`;
-        launchStatus.style.color = 'green';
+        launchStatus.style.color = 'rgb(65, 159, 106)';
     }
 
 
